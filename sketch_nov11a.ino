468019832f835494d53cdb9320aee76988e86f3f
anajuliif <126249480+anajuliif@users.noreply.github.com>
2023-11-11 14:35:14 -0300

    Add files via upload

---
+++ sketch_nov11a.ino
@@ -0,0 +1,21 @@
+#include <SPI.h>
+#include <Ethernet.h>
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+EthernetServer server(80);
+void setup() {
+Ethernet.begin(mac);
+server.begin();
+Serial.begin(9600);
+Serial.println("Servidor inicializado");
+}
+void loop() {
+EthernetClient client = server.available();
+if (client) {
+Serial.println("Novo cliente!");
+client.println("HTTP/1.1 200 OK");
+client.println("Content-Type: text/html");
+client.println();
+client.println("<html><body><h1>Hello, World!</h1></body></html>");
+client.stop();
+}
+}
